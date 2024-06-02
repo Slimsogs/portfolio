4d4aca68e2f698e5c003254c67bd2c485876812a
Slimsogs <Sogidechi16@gmail.com>
2024-06-02 18:07:06 +0100

    change

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -10,7 +10,7 @@ import Loader from 'react-loaders';
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
     const nameArray = [' ','D','a', 'n', 'i', 'e', 'l']
-    const jobArray = ['W', 'e','b',' ','D','e','v','e','l','o','p','e','r']
+    const jobArray = ['F', 'r','o','n','t','e','n','d',' ','D','e','v','e','l','o','p','e','r']
 
     useEffect(() =>{
         setTimeout(() =>{
@@ -41,7 +41,7 @@ const Home = () => {
                 idx={22}
               />
             </h1>
-            <h2>Frontend Developer</h2>
+            {/* <h2>Frontend Developer</h2> */}
             {/* <Link to="/contact" className="flat-button">
               CONTACT ME
             </Link> */}
